928973cb5ad9ccdcf8a372b0ad2b6816a7a7ccd8
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-16 18:50:21 +0800

    关闭未支付订单插件更新

---
+++ languages/zh_CN/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+
+/**
+ * 自动处理语言文件
+ */
+
+return array(
+	'unpayed' 		=> '自动关闭未付款订单',
+	'unpayed_desc' 	=> '自动关闭未付款订单',
+	'unpayed_count' => '每次关闭的个数：',
+
+	'unpayed_count_range' => array(
+		'5' 	=> '5',
+		'10' 	=> '10',
+		'20' 	=> '20',
+		'50' 	=> '50',
+	)
+);
+
+// end
